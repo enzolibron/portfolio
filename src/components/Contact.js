c244816234c2d00744d1ac337a2f58b3c219614c
Lorenzo Libron <lorenzo@bluehive.com.ph>
2023-03-07 16:48:02 +0800

    add hashrouter

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <section className="bg-secondery px-5 py-10" id="contact">
+    <section className="bg-secondery px-5 py-5" id="contact">
       <div className="text-center md:w-[60%] mx-auto">
-        <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[200px] mx-auto border-indigo-600 pb-2">
+        <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[300px] mx-auto border-indigo-600 pb-2">
           Contact Me
         </h2>
         <p>
